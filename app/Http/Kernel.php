79c3d34295e1798a160cf0f404986826177d0256
Nessa <na.nadine.001@gmail.com>
2023-12-30 02:09:12 +0700

    resolve user middleware & add admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'firebase' => \App\Http\Middleware\FirebaseMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -9,13 +9,28 @@ use Kreait\Firebase\Factory;
 use Kreait\Laravel\Firebase\Facades\Firebase;
 use Symfony\Component\HttpFoundation\Response;
 
-class FirebaseMiddleware
+class AdminMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    protected $auth, $rtdb, $firestore;
+    public function __construct()
+    {
+        $this->auth = Firebase::auth();
+
+        $firebase = (new Factory)
+            ->withServiceAccount(base_path(env('FIREBASE_CREDENTIALS')));
+
+        $this->rtdb = $firebase->withDatabaseUri(env("FIREBASE_DATABASE_URL"))
+            ->createDatabase();
+
+        $this->firestore = $firebase->createFirestore()
+            ->database();
+    }
+
     public function handle(Request $request, Closure $next): Response
     {
         $response = (object) [];
@@ -24,17 +39,25 @@ class FirebaseMiddleware
 
         $auth = app('firebase.auth');
         try {
-            if ($token == null) {
-                throw new FailedToVerifyToken('token can not be null');
-            }
-
             $verifiedIdToken = $auth->verifyIdToken($token);
-            return $next($request);
         } catch (FailedToVerifyToken $e) {
             return response()->json([
                 'message' => 'invalid token',
                 'error' => $e->getMessage()
             ]);
         }
+        $email = $verifiedIdToken->claims()->get('email');
+
+        $admins = $this->firestore->collection('admins')
+        ->where('email', '==', $email)
+        ->count();
+
+        if ($admins) {
+            return $next($request);
+        }
+
+        return response()->json([
+            'message' => 'unauthorized',
+        ]);
     }
 }
